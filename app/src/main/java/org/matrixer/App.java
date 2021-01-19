0b15f6948600d987f35dc09bfa0274caa8321c49
Patrik Bogren <pabo1800@student.miun.se>
2021-01-19 14:13:11 +0100

    Add JaCoCo library

--- app/src/main/java/org/matrixer/App.java
+++ app/src/main/java/org/matrixer/App.java
@@ -1,11 +1,21 @@
 package org.matrixer;
 
 import io.reactivex.rxjava3.core.Observable;
+import org.jacoco.core.JaCoCo;
 
 public class App {
 
     public static void main(String[] args) {
         Observable.just("App: ", "works!", "\n")
                 .subscribe(System.out::print);
+        testJaCoco();
     }
+
+    public static void testJaCoco() {
+        System.out.println("\n::JaCoCo::"
+                + "\n\tversion: " + JaCoCo.VERSION
+                + "\n\turl: " + JaCoCo.HOMEURL
+                + "\n\truntime: " + JaCoCo.RUNTIMEPACKAGE);
+    }
+
 }
