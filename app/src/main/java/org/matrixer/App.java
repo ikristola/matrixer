6ee39203ac3cd4a5849d990c6b9fefa141df7921
Isak Kristola <iskr1300@student.miun.se>
2021-01-19 12:19:26 +0100

    Formatting test fix

--- app/src/main/java/org/matrixer/App.java
+++ app/src/main/java/org/matrixer/App.java
@@ -2,7 +2,7 @@ package org.matrixer;
 
 public class App {
 
-  public static void main(String[] args) {
-    System.out.println("App: works!");
-  }
+    public static void main(String[] args) {
+        System.out.println("App: works!");
+    }
 }
