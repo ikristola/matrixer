36fc79912796ab2c173e9bc9127dc0a13e4e18b6
Patrik Bogren <mmfood@pm.me>
2021-01-19 11:45:57 +0100

    Initial commit

---
+++ app/src/main/java/org/matrixer/App.java
@@ -0,0 +1,8 @@
+package org.matrixer;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("App: works!");
+    }
+}
---
+++ app/src/test/java/org/matrixer/AppTest.java
@@ -0,0 +1,11 @@
+package org.matrixer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void appHasAGreeting() {
+        assertTrue(false);
+    }
+}
