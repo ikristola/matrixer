a8ea2e167b934eae06ea6009a77e35cc36fb444f
Patrik Bogren <pabo1800@student.miun.se>
2021-01-19 20:02:05 +0100

    Use reactive

--- app/src/main/java/org/matrixer/App.java
+++ app/src/main/java/org/matrixer/App.java
@@ -4,7 +4,15 @@ import java.io.IOException;
 
 import org.jacoco.core.analysis.Analyzer;
 import org.jacoco.core.analysis.CoverageBuilder;
+import org.jacoco.core.analysis.IClassCoverage;
+import org.jacoco.core.analysis.ICounter;
+import org.jacoco.core.analysis.ILine;
+import org.jacoco.core.analysis.IMethodCoverage;
+import org.jacoco.core.analysis.ISourceFileCoverage;
+import org.jacoco.core.analysis.ISourceNode;
 import org.jacoco.core.data.ExecutionDataStore;
+
+import io.reactivex.rxjava3.core.Observable;
 
 public class App {
 
@@ -25,11 +33,68 @@ public class App {
         var execData = new ExecutionDataStore();
         var builder = new CoverageBuilder();
         var analyzer = new Analyzer(execData, builder);
-        var count = analyzer.analyzeAll("/home/pabo/tmp/matrixer-test/build/classes/", null);
-        System.out.println("Found " + count + " class files");
 
-        for (var source : builder.getSourceFiles()) {
-            System.out.println(source.getName());
+        var path = "/home/pabo/tmp/matrixer-test/build/classes/";
+        var count = analyzer.analyzeAll(path, null);
+        System.out.println("Found " + count + " class files:");
+
+        Observable.fromIterable(builder.getSourceFiles())
+                .map(ISourceFileCoverage::getName)
+                .subscribe(System.out::println);
+
+        System.out.println("::Classes::");
+        Observable.fromIterable(builder.getClasses())
+                .filter(cls -> !isTestClass(cls))
+                .doOnNext(cls -> System.out.println(cls.getName()))
+                .flatMap(cls -> Observable.fromIterable(cls.getMethods()))
+                .subscribe(this::printMethod);
+
+    }
+
+    void printMethod(IMethodCoverage method) {
+        System.out.println("\tName: " + method.getName());
+        System.out.println("\tDesc: " + method.getDesc());
+
+        getMethodLines(method)
+                .map(ILine::getStatus)
+                .map(this::getStatusString)
+                .startWithItem("Lines:")
+                .doOnNext(System.out::println)
+                .subscribe(s -> {
+                }, e -> {
+                }, () -> System.out.println());
+    }
+
+    Observable<ILine> getMethodLines(IMethodCoverage method) {
+        return Observable.create(emitter -> {
+            var first = method.getFirstLine();
+            var last = method.getLastLine();
+            for (int i = first; i <= last; i++) {
+                ILine line = method.getLine(i);
+                emitter.onNext(line);
+            }
+            emitter.onComplete();
+        });
+    }
+
+    boolean isTestClass(IClassCoverage cls) {
+        return cls.getName()
+                .toLowerCase()
+                .endsWith("test");
+    }
+
+    String getStatusString(int status) {
+        switch (status) {
+            case ICounter.EMPTY:
+                return "Empty";
+            case ICounter.FULLY_COVERED:
+                return "Fully covered";
+            case ICounter.NOT_COVERED:
+                return "Not covered";
+            case ICounter.PARTLY_COVERED:
+                return "Partly covered";
+            default:
+                throw new RuntimeException("Not a valid status: " + status);
         }
     }
 
