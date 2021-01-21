13eb8fdcc35c0b15ced2271b54fe84f6a6ab6d3b
Patrik Bogren <pabo1800@student.miun.se>
2021-01-21 18:05:41 +0100

    Use agent with gradle tests

--- app/src/test/java/org/matrixer/AppTest.java
+++ app/src/test/java/org/matrixer/AppTest.java
@@ -4,8 +4,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+
     @Test
     void appHasAGreeting() {
+        var app = new App();
+        app.run();
+        assertTrue(true);
+    }
+
+    @Test
+    void anotherTest() {
         assertTrue(true);
     }
 }
