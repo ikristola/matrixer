982eafb5a09e5f8d3c380fb9f899ba646272a138
Patrik Bogren <pabo1800@student.miun.se>
2021-01-21 17:38:14 +0100

    Try class loader offline instrumenter

--- app/src/main/java/org/matrixer/App.java
+++ app/src/main/java/org/matrixer/App.java
@@ -1,6 +1,8 @@
 package org.matrixer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.jacoco.core.analysis.Analyzer;
 import org.jacoco.core.analysis.CoverageBuilder;
@@ -9,32 +11,92 @@ import org.jacoco.core.analysis.ICounter;
 import org.jacoco.core.analysis.ILine;
 import org.jacoco.core.analysis.IMethodCoverage;
 import org.jacoco.core.analysis.ISourceFileCoverage;
-import org.jacoco.core.analysis.ISourceNode;
 import org.jacoco.core.data.ExecutionDataStore;
+import org.jacoco.core.instr.Instrumenter;
+import org.jacoco.core.runtime.OfflineInstrumentationAccessGenerator;
 
 import io.reactivex.rxjava3.core.Observable;
 
 public class App {
 
-    public static void main(String[] args) {
-        var app = new App();
-        app.run();
+    public static class MemoryClassLoader extends ClassLoader {
+        private final Map<String, byte[]> definitions =
+                new HashMap<String, byte[]>();
+
+        public void addDefinition(final String name, final byte[] bytes) {
+            definitions.put(name, bytes);
+        }
+
+        @Override
+        protected Class<?> loadClass(final String name, final boolean resolve)
+                throws ClassNotFoundException {
+            final byte[] bytes = definitions.get(name);
+            if (bytes != null) {
+                return defineClass(name, bytes, 0, bytes.length);
+            }
+            return super.loadClass(name, resolve);
+        }
     }
+
+    public static void main(String[] args) {
+        var banana = new App();
+        banana.A();
+        // app.run();
+    }
+
+    void A() {
+        System.out.println("A()");
+        B();
+    }
+
+    void B() {
+        System.out.println("B()");
+        C();
+    }
+
+    void C() {
+        System.out.println("C()");
+        D();
+    }
+
+    void D() {
+        System.out.println("D()");
+        var stackTraceElements = Thread.currentThread().getStackTrace();
+
+        for (var elem : stackTraceElements) {
+            System.out.println(elem);
+        }
+        var len = stackTraceElements.length;
+        var origin = stackTraceElements[len - 2];
+        System.out.println("Original caller of D was: " + origin +
+                "\nThe class loader was: " + origin.getClassLoaderName() +
+                "\nThe class was: " + origin.getClassName() +
+                "\nThe method was: " + origin.getMethodName() + 
+                "\nThe filename was." + origin.getFileName() +
+                "\nThe linenbr was: " + origin.getLineNumber());
+    }
+
+
 
     private void run() {
         try {
-            analyze();
+            instrument();
+            analyze("/home/pabo/tmp/matrixer-test/build/classes/");
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
     }
 
-    private void analyze() throws IOException {
+    private void instrument() {
+        var runtime = new OfflineInstrumentationAccessGenerator();
+        var instr = new Instrumenter(runtime);
+    }
+
+    private void analyze(String path) throws IOException {
         var execData = new ExecutionDataStore();
         var builder = new CoverageBuilder();
         var analyzer = new Analyzer(execData, builder);
 
-        var path = "/home/pabo/tmp/matrixer-test/build/classes/";
         var count = analyzer.analyzeAll(path, null);
         System.out.println("Found " + count + " class files:");
 
@@ -48,7 +110,6 @@ public class App {
                 .doOnNext(cls -> System.out.println(cls.getName()))
                 .flatMap(cls -> Observable.fromIterable(cls.getMethods()))
                 .subscribe(this::printMethod);
-
     }
 
     void printMethod(IMethodCoverage method) {
@@ -58,11 +119,11 @@ public class App {
         getMethodLines(method)
                 .map(ILine::getStatus)
                 .map(this::getStatusString)
-                .startWithItem("Lines:")
-                .doOnNext(System.out::println)
+                .map(line -> "\t  " + line)
+                .startWithItem("\tLines:")
+                .doOnNext(line -> System.out.println(line))
                 .subscribe(s -> {
-                }, e -> {
-                }, () -> System.out.println());
+                }, System.err::println, System.out::println);
     }
 
     Observable<ILine> getMethodLines(IMethodCoverage method) {
