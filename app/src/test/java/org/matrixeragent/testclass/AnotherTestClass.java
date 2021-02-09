c98a3ed879be4397f58e299a447d63bb50463c4d
Isak Kristola <iskr1300@student.miun.se>
2021-02-09 16:01:58 +0100

    Classes used when testing agents

---
+++ app/src/test/java/org/matrixeragent/testclass/AnotherTestClass.java
@@ -0,0 +1,11 @@
+package org.matrixeragent.testclass;
+
+public class AnotherTestClass {
+    public boolean trueReturner() {
+        return true;
+    }
+
+    public int oneReturner() {
+        return 1;
+    }
+}
