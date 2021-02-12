5b21318bf7c9df06a4326c5a84655f269a09bc01
Isak Kristola <iskr1300@student.miun.se>
2021-02-12 11:27:01 +0100

    use polymorphism to enable subclassing of transformer

--- app/src/main/java/org/matrixeragent/Transformer.java
+++ app/src/main/java/org/matrixeragent/Transformer.java
@@ -1,16 +1,17 @@
 package org.matrixeragent;
 
-import java.io.IOException;
+import javassist.*;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
-import javassist.CannotCompileException;
-import javassist.NotFoundException;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
 
-public class Transformer implements ClassFileTransformer {
+/**
+ * Abstract base class for transforming classes using the Instrument API and Javassist.
+ * Concrete subclasses must implement the instrument method which specifies
+ * how the classes are transformed.
+ */
+public abstract class Transformer implements ClassFileTransformer {
 
     String targetClassName;
     ClassLoader targetClassLoader;
@@ -22,8 +23,8 @@ public class Transformer implements ClassFileTransformer {
 
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> cls,
-            ProtectionDomain protectionDomain,
-            byte[] classfileBuffer) {
+                            ProtectionDomain protectionDomain,
+                            byte[] classfileBuffer) {
         byte[] byteCode = classfileBuffer;
         String finalTargetClassName =
                 this.targetClassName.replaceAll("\\.", "/");
@@ -34,50 +35,26 @@ public class Transformer implements ClassFileTransformer {
 
         if (loader.equals(targetClassLoader)) {
             try {
-                System.out.println("[Agent] Transforming class " + className);
+                System.out.println("[Transformer] Transforming class " + className);
                 ClassPool pool = ClassPool.getDefault();
                 CtClass cc = pool.get(targetClassName);
                 for (var m : cc.getMethods()) {
                     if (instrument(m)) {
-                        System.out.println("[Agent] Instrumented " + m.getLongName());
+                        System.out.println("[Transformer] Instrumented " + m.getLongName());
                     }
                 }
                 byteCode = cc.toBytecode();
                 cc.detach();
                 return byteCode;
             } catch (CannotCompileException e) {
-                System.err.println("[Agent] Err Transformer.transform(): " + e.getReason());
+                System.err.println("[Transformer] Err Transformer.transform(): " + e.getReason());
             } catch (NotFoundException | IOException e) {
-                System.err.println("[Agent] Err Transformer.transform(): " + e);
+                System.err.println("[Transformer] Err Transformer.transform(): " + e);
             }
         }
         return null;
     }
 
-    private boolean instrument(CtMethod method) 
-            throws NotFoundException, CannotCompileException, IOException {
+    abstract boolean instrument(CtMethod method) throws NotFoundException, CannotCompileException, IOException;
 
-        final var name = method.getLongName();
-        if (name.startsWith("java.lang.Object")) {
-            return false;
-        }
-
-        System.out.println("[Agent] Found method: " + name);
-        StringBuilder endBlock = new StringBuilder();
-        endBlock.append(
-                "StackTraceElement[] elems = Thread.currentThread().getStackTrace();"
-                        // First StackTraceElement is getStackTrace()
-                        + "for (int i = 1; i < elems.length; i++) {"
-                        + "   StackTraceElement elem = elems[i];"
-                        + "   if (elem.getClassLoaderName() == null) {"
-                        + "       elem = elems[i-1];"
-                        + "       String caller = elem.getClassName() + \":\" + elem.getMethodName();"
-                        + "       System.out.println(\"Looks like " + name
-                        + " was called by test \" + caller);"
-                        + "       break;"
-                        + "   }"
-                        + "};");
-        method.insertAfter(endBlock.toString());
-        return true;
-    }
 }
