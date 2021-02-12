4fa45a7672643070d541f80f70a9a54e73a835cd
Isak Kristola <iskr1300@student.miun.se>
2021-02-12 11:28:26 +0100

    separate test packages for dynamic and static instrumentation

---
+++ app/src/test/java/org/matrixeragent/dynamictargets/AnotherTestClassDynamic.java
@@ -0,0 +1,11 @@
+package org.matrixeragent.dynamictargets;
+
+public class AnotherTestClassDynamic {
+    public static boolean trueReturner() {
+        return true;
+    }
+
+    public static int oneReturner() {
+        return 1;
+    }
+}
---
+++ app/src/test/java/org/matrixeragent/statictargets/AnotherTestClassStatic.java
@@ -0,0 +1,11 @@
+package org.matrixeragent.statictargets;
+
+public class AnotherTestClassStatic {
+    public static boolean trueReturner() {
+        return true;
+    }
+
+    public static int oneReturner() {
+        return 1;
+    }
+}
