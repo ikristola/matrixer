0e3fd9874832d0586335509ce60fb1e4e0773bf6
Isak Kristola <iskr1300@student.miun.se>
2021-02-12 11:30:45 +0100

    Class for capturing streams

---
+++ app/src/test/java/org/matrixeragent/util/StreamHijacker.java
@@ -0,0 +1,34 @@
+package org.matrixeragent.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Utility class for capturing data written to System.out
+ */
+public class StreamHijacker {
+
+    private final PrintStream standardOut;
+    private final ByteArrayOutputStream outputStreamCaptor;
+
+    public StreamHijacker() {
+        standardOut = System.out;
+        outputStreamCaptor = new ByteArrayOutputStream();
+    }
+
+    public void outputCapture() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    public String getHijackedOutput() {
+        return outputStreamCaptor.toString().trim();
+    }
+
+    public void stopOutputCapture() {
+        reset();
+        System.setOut(standardOut);
+    }
+    public void reset() {
+        outputStreamCaptor.reset();
+    }
+}
