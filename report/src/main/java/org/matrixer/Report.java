6cb13fd6aca228ad3a9c6f5468ab49a19d108c99
Isak Kristola <iskr1300@student.miun.se>
2021-03-12 13:24:47 +0100

    Tests for report class

--- report/src/main/java/org/matrixer/Report.java
+++ report/src/main/java/org/matrixer/Report.java
@@ -1,16 +1,19 @@
 package org.matrixer;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Report {
 
     private String dataFilePath;
     private String outputPath;
+    private String reportName;
 
     public static class Builder {
         private String dataFilePath;
-        private String outputPath;
+        private String outputPath = System.getProperty("java.io.tmpdir");
+        private String reportName = "matrixer-report.html";
 
         Builder() {}
 
@@ -32,31 +35,47 @@ public class Report {
             return this;
         }
 
+        Builder reportName(String reportName) {
+            this.reportName = reportName;
+            return this;
+        }
+
         Report build() {
             Report reportGenerator = new Report();
             reportGenerator.dataFilePath = this.dataFilePath;
             reportGenerator.outputPath = this.outputPath;
+            reportGenerator.reportName = this.reportName;
             return reportGenerator;
         }
     }
 
     private Report() {}
 
-    boolean generate() {
+    /**
+     * Generate a html report from the set raw data file.
+     * The the report is saved to the set directory path with the set file name.
+     */
+    void generate() {
+        if (!Files.exists(Path.of(dataFilePath))) {
+            throw new RuntimeException("Failed to generate report: Data file does not exist");
+        }
+
         String styleSheetPath = System.getProperty("user.dir") +
                 "/src/main/resources/report-style.css";
+
         ReportBuilder reportBuilder = new ReportBuilder(dataFilePath, styleSheetPath);
         String html = reportBuilder.generateReport();
+
         try {
             writeToFile(html);
         } catch (IOException e) {
-            System.err.println("Failed to write html to file: " + e.getMessage());
+            throw new RuntimeException("Failed to generate report. " +
+                    "Write to html file failed: " + e.getMessage());
         }
-        return true;
     }
 
     private void writeToFile(String html) throws IOException {
-        File outputFile = new File(outputPath + System.lineSeparator() + "report.html");
+        File outputFile = new File(outputPath + File.separator + reportName);
         BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
         writer.write(html);
         writer.close();
