be3b74f25f58e612f36eb0414a3349e05e31b78f
Patrik Bogren <pabo1800@student.miun.se>
2021-05-06 17:12:37 +0200

    Print test case name

--- agent/src/main/java/org/matrixer/agent/TestListener.java
+++ agent/src/main/java/org/matrixer/agent/TestListener.java
@@ -4,16 +4,20 @@ import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 
-class TestListener implements TestExecutionListener {
+public class TestListener implements TestExecutionListener {
+
+    @Override
+    public void executionStarted(TestIdentifier testIdentifier) {
+        if (testIdentifier.isTest()) {
+            System.out.println("[MATRIXER] Starting test: " + testIdentifier.getDisplayName());
+        }
+    }
 
     @Override
     public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult result) {
-        System.out.println("Ended a test");
-    }
-
-    @Override 
-    public void executionStarted(TestIdentifier testIdentifier) {
-        System.out.println("Started a test");
+        if (testIdentifier.isTest()) {
+            System.out.println("[MATRIXER] Ended test: " + testIdentifier.getDisplayName());
+        }
     }
 
 }
